1b0cdf966db505601832e4ce24d1c0f1d06bb748
Jonas Beer <cc221012@fhstp.ac.at>
2023-06-14 18:27:24 +0200

    Reformatting, security commit

--- app/models/listingModel.js
+++ app/models/listingModel.js
@@ -1,64 +1,68 @@
-const db = require('../config/database').config;
+const db = require("../config/database").config;
 
-let getAllListings = () => new Promise(async (resolve,reject)=>{
+let getAllListings = () =>
+  new Promise(async (resolve, reject) => {
     let sql = `SELECT * FROM CCL_listings`;
-    db.query(sql, function (err, listings, fields){
-        if(err){
-            reject(err);
-        }
-        resolve(listings);
+    db.query(sql, function (err, listings, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(listings);
     });
-});
+  });
 
 // get all info by a specific listing:
 // SELECT * FROM CCL_listings INNER JOIN CCL_inventory ON CCL_listings.sellerWeaponID = CCL_inventory.userWeaponID INNER JOIN CCL_weapons ON CCL_inventory.weaponID = CCL_weapons.id WHERE [key] = [value]
-let getListingByTeam = (Team) => new Promise(async (resolve,reject)=>{
-
-    let sql = "SELECT * FROM CCL_listings INNER JOIN CCL_inventory ON CCL_listings.sellerWeaponID = CCL_inventory.userWeaponID INNER JOIN CCL_weapons ON CCL_inventory.weaponID = CCL_weapons.id WHERE CCL_weapons.team = " + db.escape(Team);
+let getListingByTeam = (Team) =>
+  new Promise(async (resolve, reject) => {
+    let sql =
+      "SELECT * FROM CCL_listings INNER JOIN CCL_inventory ON CCL_listings.sellerWeaponID = CCL_inventory.userWeaponID INNER JOIN CCL_weapons ON CCL_inventory.weaponID = CCL_weapons.id WHERE CCL_weapons.team = " +
+      db.escape(Team);
     console.log(sql);
 
-    db.query(sql, function (err, listing, fields){
-        if(err){
-            reject(err);
-        }
-        resolve(listing);
+    db.query(sql, function (err, listing, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(listing);
     });
-
-});
-let addListing = (listingData) => new Promise( async (resolve,reject)=> {
-
-    let sql = "INSERT INTO CCL_listings (sellerID, sellerWeaponID, price) VALUES (" + // TODO: get the specific sellerWeaponID from the inventory table when clicking on an item in the inventory
-        db.escape(listingData.team) + ",  " +
-        db.escape(listingData.price)+ ",  "  +
-        db.escape(listingData.weaponID)+ ")" ;
+  });
+let addListing = (listingData) =>
+  new Promise(async (resolve, reject) => {
+    let sql =
+      "INSERT INTO CCL_listings (sellerID, sellerWeaponID, price) VALUES (" + // TODO: get the specific sellerWeaponID from the inventory table when clicking on an item in the inventory
+      db.escape(listingData.team) +
+      ",  " +
+      db.escape(listingData.price) +
+      ",  " +
+      db.escape(listingData.weaponID) +
+      ")";
     console.log(sql);
 
-    db.query(sql, function (err, result, fields){
-        if(err) {
-            reject(err)
-        }
-        resolve(listingData)
+    db.query(sql, function (err, result, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(listingData);
     });
+  });
 
-});
+let deleteListingById = (id) =>
+  new Promise(async (resolve, reject) => {
+    let sql = "DELETE FROM CCL_listings WHERE id = " + db.escape(id);
+    console.log(sql);
 
-let deleteListingById = (id) => new Promise( async (resolve,reject)=> {
-
-        let sql = "DELETE FROM CCL_listings WHERE id = " + db.escape(id);
-        console.log(sql);
-
-        db.query(sql, function (err, result, fields){
-            if(err) {
-                reject(err)
-            }
-            resolve() // I don't think we need to return anything here
-        });
-
-});
+    db.query(sql, function (err, result, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(); // I don't think we need to return anything here
+    });
+  });
 
 module.exports = {
-    getAllListings,
-    getListingByTeam,
-    addListing,
-    deleteListingById,
-}
+  getAllListings,
+  getListingByTeam,
+  addListing,
+  deleteListingById,
+};
--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -4,18 +4,17 @@ const userController = require("../controllers/userController.js");
 
 // TODO: Add authentication middleware to all routes
 
-    // Retrieve all Users
+// Retrieve all Users
 router.get("/", userController.getUsers);
 
-    // Create a new User
+// Create a new User
 router.post("/register", userController.registerNewUser);
 
 // Retrieve a single User with id
 router.get("/:id", userController.getUser);
 
-    // Update a User with id, previously used to render the editUser view, which then used the update the user with usercontroller.updateUser
+// Update a User with id, previously used to render the editUser view, which then used the update the user with usercontroller.updateUser
 router.put("/:id/edit", userController.editUser);
-
 
 
 module.exports = router;
