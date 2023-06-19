239daec714d2382446b5177657d8df271b0d3aef
Jonas Beer <cc221012@fhstp.ac.at>
2023-06-19 18:59:55 +0200

    formatting

--- app/models/inventoryModel.js
+++ app/models/inventoryModel.js
@@ -1,41 +1,52 @@
 //database access
 
-const db = require('../config/database').config;
+const db = require("../config/database").config;
 
-let getUserInventory = (id) => new Promise((resolve, reject) => { // the ID is the user ID
-    db.query(`SELECT CCL_inventory.userWeaponID, CCL_weapons.id, CCL_weapons.image, CCL_weapons.rarity, CCL_weapons.name FROM CCL_inventory
+let getUserInventory = (id) =>
+  new Promise((resolve, reject) => {
+    // the ID is the user ID
+    db.query(
+      `SELECT CCL_inventory.userWeaponID, CCL_weapons.id, CCL_weapons.image, CCL_weapons.rarity, CCL_weapons.name FROM CCL_inventory
             INNER JOIN CCL_weapons
             ON CCL_inventory.weaponID = CCL_weapons.id
-            WHERE CCL_inventory.userID =${id};`, function (err, inventory, fields) {
+            WHERE CCL_inventory.userID =${id};`,
+      function (err, inventory, fields) {
         if (err) {
-            reject(err);
+          reject(err);
         }
         resolve(inventory);
-    });
-});
+      }
+    );
+  });
 
-let updateSkinOwner = (sellerWeaponID, buyerID) => new Promise((resolve, reject) => {
-    db.query(`UPDATE CCL_inventory SET userID = ${buyerID} WHERE userWeaponID = ${sellerWeaponID};`, function (err, inventory, fields) {
+let updateSkinOwner = (sellerWeaponID, buyerID) =>
+  new Promise((resolve, reject) => {
+    db.query(
+      `UPDATE CCL_inventory SET userID = ${buyerID} WHERE userWeaponID = ${sellerWeaponID};`,
+      function (err, inventory, fields) {
         if (err) {
-            reject(err);
+          reject(err);
         }
         resolve(); // don't need to return anything, because only metadata would be returned
-    });
-});
+      }
+    );
+  });
 
-let getItemByUserWeaponID = (userWeaponID) => new Promise((resolve, reject) => {
-        db.query(`SELECT * FROM CCL_inventory WHERE userWeaponID = ${userWeaponID};`, function (err, item, fields) {
-            if (err) {
-                console.log('error in sql query: '+err)
-                reject(err);
-            }
-            resolve(item);
-        });
-    }
-);
+let getItemByUserWeaponID = (userWeaponID) =>
+  new Promise((resolve, reject) => {
+    const sql = `SELECT * FROM CCL_inventory INNER JOIN CCL_weapons ON CCL_inventory.weaponID = CCL_weapons.id WHERE CCL_inventory.userWeaponID = ${userWeaponID};`;
+
+    db.query(sql, function (err, item, fields) {
+      if (err) {
+        console.log("error in sql query: " + err);
+        reject(err);
+      }
+      resolve(item);
+    });
+  });
 
 module.exports = {
-    getUserInventory,
-    updateSkinOwner,
-    getItemByUserWeaponID
-}
+  getUserInventory,
+  updateSkinOwner,
+  getItemByUserWeaponID,
+};
