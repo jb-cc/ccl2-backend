718324713d528702edd716f9927e06bb39804879
JonasBeer <cc221012@fhstp.ac.at>
2023-06-20 01:09:08 +0200

    The buying works now

--- app/models/tradeModel.js
+++ app/models/tradeModel.js
@@ -1,28 +1,29 @@
 const db = require('../config/database').config;
 
 
-
-
 function handleTransaction(req, res) {
-    console.log('body: '+req.body)
     const buyerID = req.body.buyerID;
 
-    db.beginTransaction(function(err) {
+    db.beginTransaction(function (err) {
         if (err) {
-            res.status(500).json({ message: "Error starting transaction" });
+            res.status(500).json({message: "Error starting transaction"});
             return;
         }
-
-        db.query('SELECT * FROM CCL_listings WHERE sellerWeaponID = ?', [req.params.sellerWeaponID], (error, listingRows) => {
+        db.query('SELECT * FROM CCL_listings WHERE sellerWeaponID = ?', [parseInt(req.params.sellerWeaponID)], (error, listingRows) => {
             if (error) {
-                return db.rollback(function() {
-                    res.status(500).json({ message: "Error occurred while fetching listing" });
+                console.error(error);
+                return db.rollback(function () {
+                    res.status(500).json({message: "Error occurred while fetching listing"});
                 });
             }
 
             if (listingRows.length === 0) {
-                return db.rollback(function() {
-                    res.status(400).json({ message: "Listing not found" });
+                console.log('listingRows: ' + listingRows)
+                console.log('listing not found')
+                return db.rollback(function () {
+
+                    res.status(400).json({message: "Listing not found"});
+
                 });
             }
 
@@ -33,63 +34,63 @@ function handleTransaction(req, res) {
 
             db.query('SELECT * FROM CCL_users WHERE id = ?', [buyerID], (error, buyerRows) => {
                 if (error) {
-                    return db.rollback(function() {
-                        res.status(500).json({ message: "Error occurred while fetching buyer" });
+                    return db.rollback(function () {
+                        res.status(500).json({message: "Error occurred while fetching buyer"});
                     });
                 }
 
                 if (buyerRows.length === 0) {
-                    return db.rollback(function() {
-                        res.status(400).json({ message: "Buyer not found" });
+                    return db.rollback(function () {
+                        res.status(400).json({message: "Buyer not found"});
                     });
                 }
 
                 const buyer = buyerRows[0];
                 if (buyer.balance < price) {
-                    return db.rollback(function() {
-                        res.status(400).json({ message: "Buyer does not have enough money" });
+                    return db.rollback(function () {
+                        res.status(400).json({message: "Buyer does not have enough money"});
                     });
                 }
 
                 // Update the sellerWeaponID with the buyerID
                 db.query('UPDATE CCL_inventory SET userID = ? WHERE userWeaponID = ?', [buyerID, sellerWeaponID], (error) => {
                     if (error) {
-                        return db.rollback(function() {
-                            res.status(500).json({ message: "Error occurred while transferring weapon ownership" });
+                        return db.rollback(function () {
+                            res.status(500).json({message: "Error occurred while transferring weapon ownership"});
                         });
                     }
-
+                    console.log('item transaction finished')
                     // Delete the listing
                     db.query('DELETE FROM CCL_listings WHERE sellerWeaponID = ?', [sellerWeaponID], (error) => {
                         if (error) {
-                            return db.rollback(function() {
-                                res.status(500).json({ message: "Error occurred while deleting listing" });
+                            return db.rollback(function () {
+                                res.status(500).json({message: "Error occurred while deleting listing"});
                             });
                         }
 
                         // Update the buyer's and seller's money
                         db.query('UPDATE CCL_users SET balance = balance - ? WHERE id = ?', [price, buyerID], (error) => {
                             if (error) {
-                                return db.rollback(function() {
-                                    res.status(500).json({ message: "Error occurred while updating buyer's money" });
+                                return db.rollback(function () {
+                                    res.status(500).json({message: "Error occurred while updating buyer's money"});
                                 });
                             }
 
                             db.query('UPDATE CCL_users SET balance = balance + ? WHERE id = ?', [price, sellerID], (error) => {
                                 if (error) {
-                                    return db.rollback(function() {
-                                        res.status(500).json({ message:"Error occurred while updating seller's money" });
+                                    return db.rollback(function () {
+                                        res.status(500).json({message: "Error occurred while updating seller's money"});
                                     });
                                 }
 
-                                db.commit(function(err) {
+                                db.commit(function (err) {
                                     if (err) {
-                                        return db.rollback(function() {
-                                            res.status(500).json({ message: "Error committing transaction" });
+                                        return db.rollback(function () {
+                                            res.status(500).json({message: "Error committing transaction"});
                                         });
                                     }
                                     console.log('Transaction Complete.')
-                                    res.status(200).json({ message: "Transaction successful" });
+                                    res.status(200).json({message: "Transaction successful"});
                                 });
                             });
                         });
